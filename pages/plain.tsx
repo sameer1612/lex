814eccb744153652dd6800f0aa60ea846cce56df
Sameer Kumar <sameer@sedintechnologies.com>
2022-04-28 13:03:26 +0530

    Added plain text editor.

---
+++ pages/plain.tsx
@@ -0,0 +1,71 @@
+import {$getRoot, $getSelection, EditorState} from "lexical";
+import {useEffect, useState} from "react";
+
+import LexicalComposer from "@lexical/react/LexicalComposer";
+import LexicalPlainTextPlugin from "@lexical/react/LexicalPlainTextPlugin";
+import LexicalContentEditable from "@lexical/react/LexicalContentEditable";
+import LexicalOnChangePlugin from "@lexical/react/LexicalOnChangePlugin";
+import {useLexicalComposerContext} from "@lexical/react/LexicalComposerContext";
+
+const theme = {};
+
+function onChange(editorState: EditorState) {
+  editorState.read(() => {
+    const root = $getRoot();
+    const selection = $getSelection();
+
+    console.log(root, selection);
+  });
+}
+
+function MyCustomAutoFocusPlugin() {
+  const [editor] = useLexicalComposerContext();
+
+  useEffect(() => {
+    editor.focus();
+  }, [editor]);
+
+  return null;
+}
+
+function onError(error: Error) {
+  console.error(error);
+}
+
+export default function Plain() {
+  const [text, setText] = useState("");
+
+  const initialConfig = {
+    theme,
+    onError,
+  };
+
+  function updateText(editorState: EditorState) {
+    editorState.read(() => {
+      const root = $getRoot();
+
+      setText(
+        root
+          .getAllTextNodes()
+          .map((n) => n.__text)
+          .join("")
+      );
+    });
+  }
+
+  return (
+    <div className={"container my-3 lex-wrapper"}>
+      <h1 className={"text-muted mb-4 pb-2"}>Plain Text Editor</h1>
+      <LexicalComposer initialConfig={initialConfig}>
+        <LexicalPlainTextPlugin
+          contentEditable={<LexicalContentEditable/>}
+          placeholder={null}
+        />
+        <LexicalOnChangePlugin onChange={onChange}/>
+        <LexicalOnChangePlugin onChange={updateText}/>
+        <MyCustomAutoFocusPlugin/>
+        {text ? <div className="container card bg-light my-3 p-3">{text}</div> : <></>}
+      </LexicalComposer>
+    </div>
+  );
+}
---
+++ pages/rich.tsx
@@ -0,0 +1,7 @@
+export default function Rich() {
+  return (
+    <div className={"container my-3"}>
+      <h1 className={"text-muted"}>Rich Text Editor</h1>
+    </div>
+  );
+}
