d98de52631954904d649eb0b618c5b177aa9b573
Sameer Kumar <sameer@sedintechnologies.com>
2022-04-28 13:06:58 +0530

    Fixed typescript offence for norefferer on a tag.

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://lexical.dev" target={"_blank"}>Lexical!</a>
+          Welcome to <a href="https://lexical.dev" target={"_blank"} rel={"noreferrer"}>Lexical!</a>
         </h1>
         <ul className={"mt-4 text-primary"}>
           <li>
